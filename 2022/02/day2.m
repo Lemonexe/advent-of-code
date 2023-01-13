8b033928e1fec3c4e80c7f283e3b9497d790e8c0
Jiri Zbytovsky <zbytek@gmail.com>
2023-01-13 16:52:17 +0100

    2022-12-02 part 2

--- 2022/02/day2.m
+++ 2022/02/day2.m
@@ -16,19 +16,47 @@ while ischar(tline)
 end
 fclose(fileID);
 
+% FIRST PART, see calculate_total_points
+fprintf('PART 1 total score: %.0f\n', calculate_total_points(M));
 
-% NOW CALCULATE POINTS
-% points for selection of my sign
-p_choice  = sum(M(:, 2));
+% SECOND PART, see refine_M
+% actually, M is not M, because X = need to lose, Y = need to draw, Z = need to win
+fprintf('PART 2 total score: %.0f\n', calculate_total_points(refine_M(M)));
 
-% points for outcome
-m1=M(:,1);
-m2=M(:,2);
-dm = m2 - m1;
-draws = m1 == m2;
-wins = dm > 0 & dm ~= 2 | dm == -2;
-loss = dm < 0 & dm ~= -2 | dm == 2;
-p_outcome = sum(draws)*3 + sum(wins)*6;
-p_total = p_choice + p_outcome;
+% Calculate total points from the matrix M
+function p_total = calculate_total_points(M)
+    m1=M(:,1);
+    m2=M(:,2);
 
-fprintf('total score: %.0f\n', p_total);
+    % points for selection of my sign
+    p_choice  = sum(m2);
+
+    % points for outcome
+    dm = m2 - m1;
+    draws = m1 == m2;
+    wins = dm > 0 & dm ~= 2 | dm == -2;
+    loss = dm < 0 & dm ~= -2 | dm == 2;
+    p_outcome = sum(draws)*3 + sum(wins)*6;
+    
+    p_total = p_choice + p_outcome;
+end
+
+% As per updated instructions from the elf, correct what was thought to be M, to the actual M
+function new_M = refine_M(M)
+    m1=M(:,1);
+    m2=M(:,2);
+    draws_turns = m1 .* (m2==2); % my sign when it is suppossed to be draw
+
+    loss_turns = (10+m1-1) .* (m2==1);
+    loss_turns = (loss_turns > 10) .* (loss_turns - 10) + (loss_turns == 10) * 3;
+    win_turns = (10+m1+1) .* (m2==3);
+    win_turns = ((win_turns > 10) & (win_turns < 14)) .* (win_turns - 10) + (win_turns == 14) * 1;
+
+    new_m2 = loss_turns + draws_turns + win_turns;
+    new_M = [m1, new_m2];
+end
+
+%{
+    DEBUG
+    M = [1 1; 1 2; 1 3; 2 1; 2 2; 2 3; 3 1; 3 2; 3 3]
+%}
