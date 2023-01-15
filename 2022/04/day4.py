5a5daa0364b1a346e85e66cdeeaa515e2181bb58
Jiri Zbytovsky <zbytek@gmail.com>
2023-01-15 17:57:34 +0100

    2022-12-04 part 2

--- 2022/04/day4.py
+++ 2022/04/day4.py
@@ -13,12 +13,20 @@ def process_an_elf(assignment):
 def is_subset(A, B):
     return A[0] >= B[0] and A[1] <= B[1]
 
+# PART 2
+def is_overlap(A, B):
+    return (B[0] >= A[0] and B[0] <= A[1]) or (B[1] >= A[0] and B[1] <= A[1]) or is_subset(A, B)
+
+# EXECUTE
 full_subsets = 0 # in how many pairs is one of the elves subset of the other one
-with open('foo-input.txt', encoding='utf-8') as file:
+overlaps = 0 # how many pairs have any overlap
+with open('input.txt', encoding='utf-8') as file:
     for line in file:
         line = line.replace('\n', '')
         
         pair = process_pair(line)
         full_subsets += int(is_subset(pair[0], pair[1]) or is_subset(pair[1], pair[0]))
+        overlaps += int(is_overlap(pair[0], pair[1]))
 
 print(f'full subset pairs: {full_subsets}')
+print(f'overlaps: {overlaps}')
