4aa903943ec485ca582ba0918bfb408b69ae497e
Jiri Zbytovsky <zbytek@gmail.com>
2023-01-15 16:51:32 +0100

    2022-12-03 part 2

--- 2022/03/day3.py
+++ 2022/03/day3.py
@@ -1,8 +1,6 @@
-priority_sum = 0
-
+# PART 1 fns
 def process_line(line):
-    line2 = line[:-1] if line[-1] == '\n' else line
-    left, right = split(line2)
+    left, right = split(line)
     inter = find_intersect(left, right)
     priority = char_to_priority(inter)
     return priority
@@ -26,8 +24,31 @@ def char_to_priority(char):
     # a:z 97:122   A:Z 65:90
     return o - 96 if o >= 97 else o - 38
 
-sum = 0
+# PART 2 fn
+def process_group(line, lineP, linePP):
+    inter12 = find_intersect(line, lineP)
+    inter123 = find_intersect(inter12, linePP)
+    return char_to_priority(inter123)
+
+# EXECUTE
+sum = 0 # sum of items shared in both compartments
+sum_badges = 0 # sum of badges in every triplet of elves
+lineP = '' # previous file line
+linePP = '' # preprevious file line
+i = 0
 with open('input.txt', encoding='utf-8') as file:
     for line in file:
-        sum += process_line(line)
-print(sum)
+        line_cleaned = line.replace('\n', '')
+        i += 1
+        
+        # part 1
+        sum += process_line(line_cleaned)
+        
+        # part 2
+        if i % 3 == 0:
+            sum_badges += process_group(line_cleaned, lineP, linePP)
+        linePP = lineP
+        lineP = line_cleaned
+
+print(f'sum of priorities (part 1): {sum}')
+print(f'sum of badges (part 2): {sum_badges}')
