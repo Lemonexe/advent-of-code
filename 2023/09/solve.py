18e154130936dacbece6231a19b5efe71f1e66f4
Jiri Zbytovsky <zbytek@gmail.com>
2023-12-09 15:11:37 +0100

    2023-12-09 part 2

--- 2023/09/solve.py
+++ 2023/09/solve.py
@@ -11,10 +11,12 @@ lines = [np.array(line.rstrip().split(' '), dtype=np.int64) for line in lines if
 # go through list of vectors (original + diffs) to predict
 def predict(row):
     for i in range(len(row)-2, -1, -1):
-        delta = row[i][-1] + row[i+1][-1]
-        row[i] = np.append(row[i], delta)
-        # print(f'{i} {delta} {row[i]}')
-    return row[0][-1]
+        new_end = row[i][-1] + row[i+1][-1]
+        new_start = row[i][0] - row[i+1][0]
+        row[i] = np.append(row[i], new_end)
+        row[i] = np.insert(row[i], 0, new_start)
+        # print(f'{i} {new_end} {new_start} {row[i]}')
+    return row[0][-1], row[0][0]
 
 # np vector to list of vectors (original + diffs)
 def get_diffs(vec):
@@ -26,11 +28,15 @@ def get_diffs(vec):
 
 def solve():
     sum1 = 0
+    sum2 = 0
     rows = [get_diffs(vec) for vec in lines]
     for row in rows:
-        sum1 += predict(row)
+        [forwards, backwards] = predict(row)
+        sum1 += forwards
+        sum2 += backwards
 
     print(f'1st task: sum = {sum1}')
+    print(f'2st task: sum = {sum2}')
 
 
 solve()
