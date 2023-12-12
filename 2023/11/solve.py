209efcb5f8769bd414eaa408985fa3ebc1d2db7e
Jiri Zbytovsky <zbytek@gmail.com>
2023-12-12 22:39:27 +0100

    2023-12-11 part 2

--- 2023/11/solve.py
+++ 2023/11/solve.py
@@ -14,31 +14,44 @@ def expand(M):
     i = 0
     j = 0
     while i < M.shape[0]:
-        if not M[i, :].any():
-            M = np.insert(M, i, np.zeros(M.shape[1]), axis=0)
+        if not (M[i, :] == 1).any():
+            M = np.insert(M, i, np.full(M.shape[1], 2), axis=0)
             i += 1
         i += 1
     while j < M.shape[1]:
-        if not M[:, j].any():
-            M = np.insert(M, j, np.zeros(M.shape[0]), axis=1)
+        if not (M[:, j] == 1).any():
+            M = np.insert(M, j, np.full(M.shape[0], 2), axis=1)
             j += 1
         j += 1
     return M
 
 def dump(M):
-    rep = lambda x: ['.', '#'][x]
+    rep = lambda x: ['.', '#', ' '][x]
     return '\n'.join(''.join(map(rep, row)) for row in M)
 
-def solve1(M):
-    idxs = np.nonzero(M)
+# EF = expansion factor
+def solve(M, EF):
+    idxs = np.where(M == 1)
     n = len(idxs[0])
     sum1 = 0
     for i in range(n):
-        for j in range(i,n):
-            sum1 += abs(idxs[0][i]-idxs[0][j]) + abs(idxs[1][i]-idxs[1][j])
-    print(f'1st task: sum = {sum1}')
+        for j in range(i+1,n):
+            x1, y1 = idxs[0][i], idxs[1][i]
+            x2, y2 = idxs[0][j], idxs[1][j]
+            if x1 > x2: x1, x2 = x2, x1
+            if y1 > y2: y1, y2 = y2, y1
+            len_ij = x2-x1 + y2-y1
+            path_x = M[(x1+1):x2, y1]
+            path_y = M[x1, (y1+1):y2]
+            gaps_x = np.count_nonzero(path_x == 2)
+            gaps_y = np.count_nonzero(path_y == 2)
+            # I don't understand at all why EF-2, it should be EF-1 but this yields correct answer
+            sum1 += len_ij + gaps_x*(EF-2) + gaps_y*(EF-2)
+
+    print(f'With expansion factor {EF} sum = {sum1}')
 
 M = expand(M)
 print(f'M is {M.shape} after expansion')
-solve1(M)
+solve(M, 2)  # see note above
+solve(M, 1000000)
 # print(dump(M))
