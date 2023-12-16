8816c5087306cf0b517771e3dbebfc86ef089736
Jiri Zbytovsky <zbytek@gmail.com>
2023-12-16 17:18:04 +0100

    2023-12-13 part 2

--- 2023/13/solve.py
+++ 2023/13/solve.py
@@ -25,7 +25,7 @@ def investigate_V(p, p_i, L):
         if (p[:, i_C] != p[:, other(i_C)]).any():
             return 0
         i_C -= 1
-    print(f'p{p_i} {L+1}')
+    if p_i: print(f'p{p_i} {L+1}')
     return L+1
 
 # investigate pattern p, its p_i, L = i of the horizontal line
@@ -37,25 +37,43 @@ def investigate_H(p, p_i, L):
         if (p[i_R, :] != p[other(i_R), :]).any():
             return 0
         i_R -= 1
-    print(f'p{p_i} {(L+1) * H_multiplier}')
+    if p_i: print(f'p{p_i} {(L+1) * H_multiplier}')
     return L+1
 
-def solve_pattern(p, p_i):
+def solve_pattern(p, p_i, ignore=0):
     n_R, n_C = p.shape
     sum_p = 0
     for i_C in range(n_C-1):
         if (p[:, i_C] == p[:, i_C+1]).all():
             # print(f'p{p_i} ?V@{i_C + 1}')
-            sum_p += investigate_V(p, p_i, i_C)
+            num = investigate_V(p, p_i, i_C)
+            if num != ignore: sum_p += num
     for i_R in range(n_R-1):
         if (p[i_R, :] == p[i_R+1, :]).all():
             # print(f'p{p_i} ?H@{i_R + 1}')
-            sum_p += investigate_H(p, p_i, i_R) * H_multiplier
+            num = investigate_H(p, p_i, i_R) * H_multiplier
+            if num != ignore: sum_p += num
     return sum_p
 
-def solve1():
-    sum_all = 0
+def find_alt_pattern(p, p_i):
+    n_R, n_C = p.shape
+    for i_R in range(n_R):
+        for i_C in range(n_C):
+            p_alt = p.copy()
+            p_alt[i_R, i_C] = 1 - p_alt[i_R, i_C]
+            num0 = solve_pattern(p, None)
+            num = solve_pattern(p_alt, None, ignore=num0)
+            if num > 0:
+                print(f'-p{p_i} {num}')
+                return num
+    return 0
+
+def solve():
+    sum_1 = 0
+    sum_2 = 0
     for p_i,p in enumerate(patterns):
-        sum_all += solve_pattern(p, p_i)
-    print(f'1st task num = {sum_all}')
-solve1()
+        sum_1 += solve_pattern(p, p_i+1)
+        sum_2 += find_alt_pattern(p, p_i+1)
+    print(f'1st task num = {sum_1}')
+    print(f'2nd task num = {sum_2}')
+solve()
